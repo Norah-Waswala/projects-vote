bb5bda79d56185b65e6c666e40d1205e1f47d203
Norah.Waswala <norah.waswala@student.moringa.com>
2022-06-15 08:55:54 +0300

    feat:define api view functions

--- votesapp/views.py
+++ votesapp/views.py
@@ -1,71 +1,117 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from .forms import SignupForm, PostForm, UpdateUserForm, UpdateUserProfileForm, RatingsForm
+from .forms import SignupForm, PostForm, DetailsForm,UpdateUserForm, UpdateUserProfileForm, RatingsForm
 from rest_framework import viewsets
 from .models import Profile, Post, Rating
-from .serializers import ProfileSerializer, UserSerializer, PostSerializer
+from .serializers import ProfileSerializer, PostSerializer
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 import random
 from django.contrib.auth import authenticate, login
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 
 
-def signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('index')
-    else:
-        form = SignupForm()
-    return render(request, 'registration/signup.html', {'form': form})
-        
-def index(request):
+def signin(request):
+    if request.method=="POST":
+        username=request.POST["username"]
+        password=request.POST["password"]
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,"You have successfuly loged in")
+            return redirect ("index")
+    return render(request,'registration/login.html')
+def register(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.user = request.user
-            post.save()
-    else:
-        form = PostForm()
+        username=request.POST["username"]
+       
+        email=request.POST["email"]
+        password1=request.POST["password1"]
+        password2=request.POST["password2"]
+        if password1 !=password2:
+            messages.error(request,'Password do not match')
+            return render('registration/register')
+        new_user=User.objects.create_user(
+            username=username,
+           
+            email=email,
+            password=password1,
+        )
+        new_user.save()
+        return render (request,'registration/login.html')
+    return render(request,'registration/signup.html')
 
-    try:
-        posts = Post.objects.all()
+def index(request):
+    posts = Post.objects.all()
+    current_user = request.user 
+    
+    if request.method=='POST':
+        details_form = DetailsForm(request.POST, request.FILES)
+        posts_form = PostForm(request.POST, request.FILES)
+        
+        if details_form.is_valid():
+            profile = details_form.save(commit=False)
+            profile.user = current_user
+            profile.save()
+            
+        if posts_form.is_valid():
+            post = posts_form.save(commit=False)
+            post.profile = current_user.profile
+            posts_form.save()
+            
+        return redirect('index')
+        
+    else:
+        details_form = DetailsForm
+        posts_form = PostForm
+        
+    return render(request,'index.html', {'details_form':details_form, 'posts_form':posts_form, 'posts':posts,})
+# def index(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.user = request.user
+#             post.save()
+#     else:
+#         form = PostForm()
+
+#     try:
+#         posts = Post.objects.all()
         # posts = posts[::-1]
         # a_post = random.randint(0, -1)
         # random_post = posts[a_post]
-        # print(random_post.photo)
-    except Post.DoesNotExist:
-        posts = None
-    return render(request, 'index.html')
+#         # print(random_post.photo)
+#     except Post.DoesNotExist:
+#         posts = None
+#     return render(request, 'index.html')
 
 
-class ProfileViewSet(viewsets.ModelViewSet):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
+# class ProfileViewSet(viewsets.ModelViewSet):
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserViewSet(viewsets.ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+# class PostViewSet(viewsets.ModelViewSet):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
 
 
 
 
 @login_required(login_url='login')
 def profile(request, username):
+    posts = Post.objects.all()
+  
     return render(request, 'profile.html')
 
 
@@ -142,7 +188,7 @@ def project(request, post):
         'rating_status': rating_status
 
     }
-    return render(request, 'project.html', params)
+    return render(request, 'single.html', params)
 
 
 def search_project(request):
@@ -155,7 +201,20 @@ def search_project(request):
             'results': results,
             'message': message
         }
-        return render(request, 'results.html', params)
+        return render(request, 'search.html', params)
     else:
         message = "You haven't searched for any image category"
-    return render(request, 'results.html', {'message': message})
+    return render(request, 'search.html', {'message': message})
+
+class PostList(APIView):
+    def get(self, request, format=None):
+        all_post = Post.objects.all()
+        serializers = PostSerializer(all_post, many=True)
+        return Response(serializers.data)
+
+
+class ProfileList(APIView):
+    def get(self, request, format=None):
+        all_profile = Profile.objects.all()
+        serializers = ProfileSerializer(all_profile, many=True)
+        return Response(serializers.data)

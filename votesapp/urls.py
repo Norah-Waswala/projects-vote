571b5e32c2823226521a9f2cc7621b253e39ba2d
Norah.Waswala <norah.waswala@student.moringa.com>
2022-06-15 08:54:53 +0300

    feat: add url path to profile and api files

--- votesapp/urls.py
+++ votesapp/urls.py
@@ -11,15 +11,17 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     
-    path('signup/', views.signup, name='signup'),
-    path('account/', include('django.contrib.auth.urls')),
-    # path('api/', include(router.urls)),
+    path('login/',views.signin,name='login'),
+    path('register/',views.register,name='register'),
+    path('profile/',views.profile, name = 'profile'),
     path('<username>/profile', views.user_profile, name='userprofile'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('profile/<username>/', views.profile, name='profile'),
     path('profile/<username>/settings', views.edit_profile, name='edit'),
     path('project/<post>', views.project, name='project'),
     path('search/', views.search_project, name='search'),
+    path('api/post/',views.PostList.as_view()),
+    path('api/profile/', views.ProfileList.as_view()),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
